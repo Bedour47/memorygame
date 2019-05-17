74fb1bc1428c8cfa98747d8a7a868058fb5b90fc
Bedour <Bedour@users.noreply.github.com>
2019-05-17 06:58:34 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; //This array will eventually hold the cards that are in play.
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+//if statement that checks to see if the length of the cardsInPlay array is 2.
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+}
