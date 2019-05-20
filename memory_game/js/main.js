34f0e7414d94fbee9cb5dc6a9f6a3e4ffa0e70bc
Bedour <Bedour@users.noreply.github.com>
2019-05-20 05:26:35 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = []; //This array will eventually hold the cards that are in play.
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-
-//if statement that checks to see if the length of the cardsInPlay array is 2.
-if (cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
 	}
-	else{
-		alert("Sorry, try again.");
+}//end of checkForMatch function
+
+function flipCard (cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]); //add the card that the user flipped to the cardsInPlay array.
+
+	//if statement that checks to see if the length of the cardsInPlay array is 2 to call checkForMatch.
+	if (cardsInPlay.length === 2){
+		checkForMatch();
 	}
-}
+}//end of flipCard function
+
+flipCard(0);
+flipCard(2);
