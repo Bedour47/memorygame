90aaaa8b81dd6d97f610fa412871699f7ede07a9
Bedour <Bedour@users.noreply.github.com>
2019-05-31 09:21:17 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,22 +25,53 @@ var cardsInPlay = []; //This array will eventually hold the cards that are in pl
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  		console.log("You found a match!");//alert?
+  		alert("You found a match!");
 	} else {
   		alert("Sorry, try again.");
 	}
-}//end of checkForMatch function
+};//end of checkForMatch function
 
-function flipCard (cardId){
-	console.log("User flipped " + cards[cardId].rank);
+
+function flipCard (){
+	//getAttribute method to get the data-id attribute of the card that was just clicked and store it in a variable cardId.
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank); //add the card that the user flipped to the cardsInPlay array.
-	console.log(cards[cardId].cardImage);//temp?
-	console.log(cards[cardId].suit);//temp?
+	//log into the console the flipped card details.
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	//use the setAttribute method to update the src attribute to the image of the card that was just clicked
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	//if statement that checks to see if the length of the cardsInPlay array is 2 to call checkForMatch.
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 	}
-}//end of flipCard function
+};//end of flipCard function
 
-flipCard(0);
-flipCard(2);
+
+//this function will create a new game board
+function createBoard (){
+	for (var i = 0; i < cards.length; i++) {
+		//For each card, use createElement to create an "img" element and store it in a variable cardElement.
+		var cardElement = document.createElement('img');
+		//use the setAttribute() method on cardElement to set some attributes that we want. (here src attribute)
+		cardElement.setAttribute('src', 'images/back.png');
+		//Set the card's 'data-id' attribute to be the current index of the card array, (i).
+		cardElement.setAttribute('data-id', i);
+		
+		//use the addEventListener() method on the cardElement. Add a click event so that when a user clicks on a card, the flipCard function is called.
+		cardElement.addEventListener('click', flipCard);
+		//append the cardElement to the game board.
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};//end of createBoard function
+
+createBoard();
+
+//this function will resets the game board
+function reset(){
+	document.getElementById('game-board').innerHTML = " "; //to clear the game board.
+	cardsInPlay = []; //to clear the array content so the alert will be effective again once we reset the board.
+	createBoard(); //to create a new game board :)
+}
